d2e338739d290c936acc647bd10acd0ca2510aeb
GonzaMato <gonzalo.mato@ing.austral.edu.ar>
2025-08-24 20:24:44 -0300

    fixed Mockbean

--- src/test/kotlin/com/notivest/pricefetcher/controllers/HistoricalControllerTest.kt
+++ src/test/kotlin/com/notivest/pricefetcher/controllers/HistoricalControllerTest.kt
@@ -10,8 +10,8 @@ import org.mockito.kotlin.any
 import org.mockito.kotlin.whenever
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
@@ -23,7 +23,7 @@ class HistoricalControllerTest {
   @Autowired
   private lateinit var mockMvc: MockMvc
 
-  @MockBean
+  @MockitoBean
   private lateinit var marketDataService: MarketDataService
 
   @Test
--- src/test/kotlin/com/notivest/pricefetcher/controllers/WatchListControllerTest.kt
+++ src/test/kotlin/com/notivest/pricefetcher/controllers/WatchListControllerTest.kt
@@ -8,8 +8,8 @@ import org.mockito.kotlin.doThrow
 import org.mockito.kotlin.whenever
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
@@ -19,7 +19,7 @@ class WatchListControllerTest {
   @Autowired
   private lateinit var mockMvc: MockMvc
 
-  @MockBean
+  @MockitoBean
   private lateinit var watchListService: WatchListService
 
   @Test
